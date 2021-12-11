221eff65ecca5a9091f147a79c8d66e9fe0ddb6a
usama9998 <95363051+usama9998@users.noreply.github.com>
2021-12-11 22:49:06 +0500

    Add files via upload

---
+++ python_scheduled_message.py
@@ -0,0 +1,10 @@
+from email import message
+import pywhatkit
+
+###### for automating WhatsAPp message
+
+# pywhatkit.sendwhatmsg('+923171830109','Hello its scheduled again', 21,10)
+
+##### for playing youtube video
+
+pywhatkit.playonyt("PyWhatKit")
